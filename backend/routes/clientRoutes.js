d920d0b4529d4116ab515fa05cb362106bd6d48f
Diego Rios <73723371+dandresrp@users.noreply.github.com>
2025-03-15 10:51:37 -0600

    refactor(backend): streamline middleware imports and enhance route definitions for client operations

--- backend/routes/clientRoutes.js
+++ backend/routes/clientRoutes.js
@@ -1,14 +1,13 @@
 import express from 'express'
-import { authenticateJWT } from '../middlewares/auth.js'
-import { authorizeAdmin } from '../middlewares/authAdmin.js'
+import { authenticateJWT, authorizeAdmin } from '../middlewares/auth.js'
 import * as clientController from '../controllers/clientController.js'
 
 const router = express.Router()
 
 router.get('/', authenticateJWT, clientController.getAllClients)
 router.get('/:id_cliente', authenticateJWT, clientController.getClientById)
-router.post('/',authenticateJWT, authorizeAdmin, clientController.addClient)
-router.put('/:id_cliente', authenticateJWT,  authorizeAdmin, clientController.updateClient)
-router.delete('/:id_cliente',authenticateJWT, authorizeAdmin, clientController.deleteClient)
+router.post('/', [authenticateJWT, authorizeAdmin], clientController.addClient)
+router.put('/:id_cliente', [authenticateJWT, authorizeAdmin], clientController.updateClient)
+router.delete('/:id_cliente',[authenticateJWT, authorizeAdmin], clientController.deleteClient)
 
 export default router
