ab6785306885f2b94c0767a5a10a228b70e97c34
Diego Rios <73723371+dandresrp@users.noreply.github.com>
2025-04-09 13:23:18 -0600

    feat: enhance user and client management modals with loading states and dynamic data fetching

--- backend/controllers/user/sql.js
+++ backend/controllers/user/sql.js
@@ -1,14 +1,14 @@
 export const SQL_GET_ALL_USERS = `
-SELECT id_usuario, nombre_usuario, nombre, correo, rol FROM usuarios
+SELECT usuario_id, nombre_usuario, nombre, correo, rol FROM usuarios
 `;
 export const SQL_GET_USER_BY_ID = `
-SELECT id_usuario, nombre_usuario FROM usuarios WHERE id_usuario = $1
+SELECT usuario_id, nombre_usuario FROM usuarios WHERE usuario_id = $1
 `;
 
 export const SQL_UPDATE_USER = `
-UPDATE usuarios SET nombre_usuario = $1 WHERE id_usuario = $2
+UPDATE usuarios SET nombre_usuario = $1 WHERE usuario_id = $2
 `;
 
 export const SQL_DELETE_USER = `
-DELETE FROM usuarios WHERE id_usuario = $1
+DELETE FROM usuarios WHERE usuario_id = $1
 `;
